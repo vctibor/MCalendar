0879079056e94912ec941cfbfae7e30e472480e7
Vladimír Ctibor <vladimir.ctibor@samo.cz>
2018-12-06 13:43:55 +0100

    Systemd unit and install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/sh
+
+# REQUIRES ROOT PRIVILIGES
+
+echo "Clean build directory"
+rm -rf target
+
+echo "Build release version"
+cargo build --release
+
+echo "Init .rpm builder"
+cargo rpm init
+
+echo "Build .rpm package"
+cargo rpm build
+
+echo "Stop systemd service"
+systemctl stop mcalendar.service
+
+echo "Remove systemd service"
+systemctl delete mcalendar.service
+
+echo "Remove systemd unit file"
+rm /lib/systemd/system/mcalendar.service
+
+echo "Uninstall old package"
+yum remove -y mcalendar
+
+echo "Install new package"
+yum install -y target/release/rpmbuild/RPMS/x86_64/*
+
+echo "Copy new systemd unit file"
+cp mcalendar.service /lib/systemd/system/mcalendar.service
+
+echo "Start systemd service"
+systemctl start mcalendar.service
