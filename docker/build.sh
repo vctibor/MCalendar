38b9bc8d5527697e4d87cb0f998618ffe67186d3
Vladimir Malky <vladimir.malky@gmail.com>
2019-05-08 08:57:44 +0200

    Changes to docker build proces

--- docker/build.sh
+++ docker/build.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-sudo rm -rf /home/malky/mcalendar.sled
-
 cargo build --release
 
 cp ../target/release/mcalendar .
-
-# cp ../example_config.toml config.toml
 
 cp -r ../static .
 
 cp -r ../templates .
 
 tar -cf mcalendar.tar mcalendar config.toml static templates
-
-scp -r malky@192.168.1.2:/home/malky/mcalendar.sled /home/malky/
 
 docker build -t mcalendar .
 
@@ -26,9 +20,7 @@ rm -rf ./mcalendar.tar
 docker rm -f -v mcal
 
 docker run --name mcal --restart unless-stopped \
-	-p 8000:8000 -d \
-	-v /home/malky/mcalendar.sled:/var/mcalendar/mcalendar.sled \
-	mcalendar
+	-p 8000:8000 -d mcalendar
 
 sudo firewall-cmd --zone=public --add-port=8000/tcp --permanent
 
