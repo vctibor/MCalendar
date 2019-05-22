d197aa2edfb8cef704574f95b5af89b7681a51c0
Malky <vladimir.malky@gmail.com>
2019-05-22 07:00:18 +0200

    Modified DOCKERFILE

--- docker/build.sh
+++ docker/build.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-sudo rm -rf /home/malky/mcalendar.sled
-
 cargo build --release
 
 cp ../target/release/mcalendar .
-
-# cp ../example_config.toml config.toml
 
 cp -r ../static .
 
 cp -r ../templates .
 
 tar -cf mcalendar.tar mcalendar config.toml static templates
-
-scp -r malky@192.168.1.2:/home/malky/mcalendar.sled /home/malky/
 
 docker build -t mcalendar .
 
@@ -27,7 +21,6 @@ docker rm -f -v mcal
 
 docker run --name mcal --restart unless-stopped \
 	-p 8000:8000 -d \
-	-v /home/malky/mcalendar.sled:/var/mcalendar/mcalendar.sled \
 	mcalendar
 
 sudo firewall-cmd --zone=public --add-port=8000/tcp --permanent
